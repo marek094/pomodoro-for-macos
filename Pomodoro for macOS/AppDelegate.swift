0b604f2ac7ada768d9582d653c502a7128e73a78
Marek Cerny <me@marekcerny.com>
2016-09-03 18:44:43 +0200

    Initial Commit

---
+++ Pomodoro for macOS/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Pomodoro for macOS
+//
+//  Created by Marek Černý on 03/09/16.
+//  Copyright © 2016 Marek Cerny. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
